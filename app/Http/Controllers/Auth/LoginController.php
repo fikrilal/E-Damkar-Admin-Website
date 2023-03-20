0e0b70ba748d59f8034ba21730515aafe600f7a4
fikrilal <fikrimuzakky24@gmail.com>
2023-03-20 10:19:27 +0700

    fix logic login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,9 +6,29 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+
+    public function login(Request $request){
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ?'email' : 'username';
+
+        $login = [
+            $loginType => $request->username,
+            'password' => $request->password
+        ];
+
+        if(auth()->attempt($login)){
+            return redirect()->route('home');
+        }
+        return redirect()->route('login')->with(['error' => 'Email/Password salah !']);
+    }
     /*
     |--------------------------------------------------------------------------
     | Login Controller
@@ -19,26 +39,6 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-
-    public function login(Request $request)
-    {
-        $this->validate($request, [
-            'username' => 'required|string',
-            'password' => 'required|string|min:6',
-        ]);
-
-        $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' :'username';
-
-        $login = [
-            $loginType => $request->username,
-            'password' => $request->password
-        ];
-
-        if(auth()->attempt($login)){
-            return redirect()->route('home');
-        }
-        return redirect()->route('login')->with(['error' => 'Email/Password salah !']);
-    }
 
     use AuthenticatesUsers;
 
@@ -54,6 +54,8 @@ class LoginController extends Controller
      *
      * @return void
      */
+
+     
     public function __construct()
     {
         $this->middleware('guest')->except('logout');

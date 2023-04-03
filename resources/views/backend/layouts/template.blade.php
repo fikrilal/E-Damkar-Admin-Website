c8530afd97cdb3b36566960b120a54c7ed9bd457
ahmadansori1 <e41210577@student.polije.ac.id>
2023-04-03 10:21:37 +0700

    Update template.blade.php

--- resources/views/backend/layouts/template.blade.php
+++ resources/views/backend/layouts/template.blade.php
@@ -66,13 +66,13 @@
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="{{ asset('backend/assets/img/profile-img.jpg')}}" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2">Akhdan Maulana</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Kevin Anderson</h6>
-              <span>Web Designer</span>
+              <h6>Akhdan Maulana</h6>
+              <span>Administrastor</span>
             </li>
             <li>
               <hr class="dropdown-divider">
@@ -81,17 +81,7 @@
             <li>
               <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i class="bi bi-person"></i>
-                <span>My Profile</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                <i class="bi bi-gear"></i>
-                <span>Account Settings</span>
+                <span>Pengaturan Profil</span>
               </a>
             </li>
             <li>
@@ -101,7 +91,7 @@
             <li>
               <a class="dropdown-item d-flex align-items-center" href="#">
                 <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
+                <span>Keluar</span>
               </a>
             </li>
 

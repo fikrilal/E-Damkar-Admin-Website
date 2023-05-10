68bd2e09d7db327b7177960e26bbc7716d07cafc
ahmadansori1 <e41210577@student.polije.ac.id>
2023-05-10 13:44:12 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,15 +3,6 @@
 
 <main id="main" class="main">
 
-
-@if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-    {{ session('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>
-    @endif
-
-
     <div class="pagetitle">
       <h1>Dashboard</h1>
       <nav>
@@ -21,6 +12,13 @@
         </ol>
       </nav>
     </div><!-- End Page Title -->
+
+    @if (session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+    @endif
 
 
     <section class="section dashboard">

8d620a90dd579c366034ed40e643232cd7fe25cd
ahmadansori1 <e41210577@student.polije.ac.id>
2023-05-17 20:24:13 +0700

    UPDATE

--- resources/views/backend/edukasi.blade.php
+++ resources/views/backend/edukasi.blade.php
@@ -147,7 +147,7 @@
                 <div class="modal-dialog modal-xl">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h5 class="modal-title">Update Edukasi</h5>
+                      <h5 class="modal-title">Update Agenda</h5>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">

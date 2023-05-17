e55341a692ba6abcb1b3b80e6df21f9eacd2f71a
ahmadansori1 <e41210577@student.polije.ac.id>
2023-05-17 15:08:59 +0700

    update

--- resources/views/landinginformasi/landingagenda.blade.php
+++ resources/views/landinginformasi/landingagenda.blade.php
@@ -1,45 +1,51 @@
-
 @extends('frontend/layouts.template')
-    @section('content')
 
-    <main id="main">
+@section('content')
 
-        <!-- ======= Recent Blog Posts Section ======= -->
-        <section id="recent-blog-posts" class="recent-blog-posts">
+<main id="main">
 
-            <div class="container" data-aos="fade-up">
+    <!-- ======= Recent Blog Posts Section ======= -->
+    <section id="recent-blog-posts" class="recent-blog-posts">
 
-                <header class="section-header">
-                    <h2></h2>
-                    <p>Artikel Agenda</p>
-                </header>
+        <div class="container" data-aos="fade-up">
 
-                <div class="box">
-                    <div class="container-1">
-                        <span class="icon"><i class="bi bi-search"></i></span>
-                        <input type="search" id="search" class="rounded-pill" placeholder="Cari artikel agenda"/>
-                    </div>
+            <header class="section-header">
+                <h2></h2>
+                <p>Artikel Agenda</p>
+            </header>
+
+            <div class="box">
+                <div class="container-1">
+                    <span class="icon"><i class="bi bi-search"></i></span>
+                    <form action="{{ route('landingagenda.index') }}" method="GET"> <!-- Form pencarian -->
+                        <input type="search" name="search" id="search" class="rounded-pill" placeholder="Cari artikel agenda" value="{{ $search ?? '' }}"/>
+                    </form>
                 </div>
-
-                <div class="row">
-                @foreach($artikel as $item)
-                    <div class="col-lg-4 p-3">
-                        <div class="post-box">
-                            <div class="post-img"><img src="{{ asset('img-agenda/' . $item->foto_artikel_agenda) }}" class="img-fluid" alt="{{ $item->judul_agenda }}" style="width: 1000px; height: 400px;"></div>
-                            <span class="post-date">{{ \Carbon\Carbon::parse($item->tgl_agenda)->locale('id')->isoFormat('dddd, D MMMM YYYY') }}</span>
-                            <h3 class="cc">{{ $item->judul_agenda }}</h3>
-                            <p>{{ $item->deskripsi }}</p>
-                            <a href="{{ route('detailagenda.show', ['id_agenda' => $item->id_agenda]) }}" class="readmore stretched-link mt-auto"><span>Baca Selengkapnya</span><i
-                                    class="bi bi-arrow-right"></i></a>
-                        </div>
-                    </div>
-                    @endforeach
-                </div>
-
             </div>
 
-        </section><!-- End Recent Blog Posts Section -->
+            <div class="row">
+                @forelse($artikel as $item)
+                <div class="col-lg-4 p-3">
+                    <div class="post-box">
+                        <div class="post-img"><img src="{{ asset('img-agenda/' . $item->foto_artikel_agenda) }}" class="img-fluid" alt="{{ $item->judul_agenda }}" style="width: 1000px; height: 400px;"></div>
+                        <span class="post-date">{{ \Carbon\Carbon::parse($item->tgl_agenda)->locale('id')->isoFormat('dddd, D MMMM YYYY') }}</span>
+                        <h3 class="cc">{{ $item->judul_agenda }}</h3>
+                        <p>{{ $item->deskripsi }}</p>
+                        <a href="{{ route('detailagenda.show', ['id_agenda' => $item->id_agenda]) }}" class="readmore stretched-link mt-auto"><span>Baca Selengkapnya</span><i
+                                class="bi bi-arrow-right"></i></a>
+                    </div>
+                </div>
+                @empty
+                <div class="col-lg-12 text-center">
+                    <p>Tidak ada artikel yang ditemukan.</p>
+                </div>
+                @endforelse
+            </div>
 
 
-    </main><!-- End #main -->
+        </div>
+
+    </section><!-- End Recent Blog Posts Section -->
+
+</main><!-- End #main -->
 @endsection

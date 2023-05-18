bc60048145a14298c7d20ab48dfccab1b41fba64
ahmadansori1 <e41210577@student.polije.ac.id>
2023-05-18 19:32:11 +0700

    update sidebar

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -4,16 +4,16 @@
 <ul class="sidebar-nav" id="sidebar-nav">
 
   <li class="nav-item">
-    <a class="nav-link collapsed <?php echo (Request::is('dashboard')) ? 'active' : ''; ?>" href="{{ url('dashboard')}}">
+  <a class="nav-link <?php echo (Request::is('dashboard')) ? '' : 'collapsed'; ?>" href="{{ url('dashboard')}}">
         <i class="bi bi-grid"></i>
         <span>Dashboard</span>
     </a>
 </li><!-- End Dashboard Nav -->
 
-<li class="nav-item <?php echo (Request::is('laporanmasuk') || Request::is('laporan')) ? 'active' : ''; ?>">
-    <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-journal-text"></i><span>Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
-    </a>
+<li class="nav-item">
+<a class="nav-link <?php echo (Request::is('laporan') || Request::is('laporanmasuk')) ? '' : 'collapsed'; ?>" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
+    <i class="bi bi-journal-text"></i><span>Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
+</a>
     <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
         <li>
             <a href="{{ url('laporanmasuk')}}" class="<?php echo (Request::is('laporanmasuk')) ? 'active' : ''; ?>">
@@ -30,35 +30,36 @@
 
 
 <li class="nav-item">
-    <a class="nav-link collapsed <?php echo (Request::is('berita*')) ? 'active' : ''; ?>" href="{{ url('berita')}}">
+    <a class="nav-link <?php echo (Request::is('berita')) ? '' : 'collapsed'; ?>" href="{{ url('berita')}}">
         <i class="bi bi-layout-text-window-reverse"></i><span>Berita</span>
     </a>
 </li><!-- End Tables Nav -->
 
 <li class="nav-item">
-    <a class="nav-link collapsed <?php echo (Request::is('edukasi*')) ? 'active' : ''; ?>" href="{{ url('edukasi')}}">
+    <a class="nav-link <?php echo (Request::is('edukasi')) ? '' : 'collapsed'; ?>" href="{{ url('edukasi')}}">
         <i class="bi bi-book"></i><span>Edukasi</span>
     </a>
 </li><!-- End Charts Nav -->
 
 <li class="nav-item">
-    <a class="nav-link collapsed <?php echo (Request::is('agenda*')) ? 'active' : ''; ?>" href="{{ url('agenda')}}">
+    <a class="nav-link <?php echo (Request::is('agenda')) ? '' : 'collapsed'; ?>" href="{{ url('agenda')}}">
         <i class="bi bi-calendar-event"></i><span>Agenda</span>
     </a>
 </li><!-- End Charts Nav -->
+
 @if($kedudukans_id == 1)
     <li class="nav-item">
-        <a class="nav-link collapsed <?php echo (Request::is('pengaturan*') || Request::is('kelolaadmin*')) ? 'active' : ''; ?>" data-bs-target="#icons-setting" data-bs-toggle="collapse" href="#">
+        <a class="nav-link <?php echo (Request::is('pengaturan') || Request::is('kelolaadmin')) ? '' : 'collapsed'; ?>" data-bs-target="#icons-setting" data-bs-toggle="collapse" href="#">
             <i class="bi bi-gear"></i><span>Pengaturan</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
         <ul id="icons-setting" class="nav-content collapse" data-bs-parent="#sidebar-nav">
             <li>
-                <a href="{{ url('pengaturan')}}" class="<?php echo (Request::is('pengaturan*')) ? 'active' : ''; ?>">
+                <a href="{{ url('pengaturan')}}" class="<?php echo (Request::is('pengaturan')) ? 'active' : ''; ?>">
                     <i class="bi bi-circle"></i><span>Data</span>
                 </a>
             </li>
             <li>
-                <a href="{{ url('kelolaadmin')}}" class="<?php echo (Request::is('kelolaadmin*')) ? 'active' : ''; ?>">
+                <a href="{{ url('kelolaadmin')}}" class="<?php echo (Request::is('kelolaadmin')) ? 'active' : ''; ?>">
                     <i class="bi bi-circle"></i><span>Admin</span>
                 </a>
             </li>
@@ -66,7 +67,7 @@
     </li><!-- End Icons Nav -->
 @elseif($kedudukans_id == 2)
     <li class="nav-item">
-        <a class="nav-link collapsed <?php echo (Request::is('pengaturan*')) ? 'active' : ''; ?>" href="{{ url('pengaturan')}}">
+        <a class="nav-link <?php echo (Request::is('pengaturan')) ? '' : 'collapsed'; ?>" href="{{ url('pengaturan')}}">
             <i class="bi bi-gear"></i>
             <span>Pengaturan</span>
         </a>
@@ -74,7 +75,7 @@
 @endif
 
 <li class="nav-item">
-    <a class="nav-link collapsed <?php echo (Request::is('logout')) ? 'active' : ''; ?>" href="#" data-bs-toggle="modal" data-bs-target="#largeModalLog">
+    <a class="nav-link collapsed" href="#" data-bs-toggle="modal" data-bs-target="#largeModalLog">
         <i class="bi bi-box-arrow-right"></i>
         <span>Keluar</span>
     </a>

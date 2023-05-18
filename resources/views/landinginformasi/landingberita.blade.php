8cc4d7c8322c0ae14d45c57651296dbc60fd022b
ahmadansori1 <e41210577@student.polije.ac.id>
2023-05-18 18:17:13 +0700

    update

--- resources/views/landinginformasi/landingberita.blade.php
+++ resources/views/landinginformasi/landingberita.blade.php
@@ -18,16 +18,15 @@
                     <div class="icon">
                         <i class="bi bi-search"></i>
                     </div>
-                    <form action="{{ route('landingberita.index') }}" method="GET">
-                        <input type="search" name="search" id="search" class="rounded-pill" placeholder="Cari artikel berita" value="{{ $search ?? '' }}">
+                    <form action="{{ route('landingedukasi.index') }}" method="GET">
+                        <input type="search" name="search" id="search" class="rounded-pill" placeholder="Cari artikel edukasi" value="{{ $search ?? '' }}">
                     </form>
                 </div>
-
+                
                 <div class="row">
                     @forelse($artikel as $item)
                     <div class="col-lg-4 p-3">
                         <div class="post-box">
-<<<<<<< HEAD
                             <div class="post-img"><img src="{{ asset('img-berita/' . $item->foto_artikel_berita) }}" class="img-fluid" 
                             alt="{{ $item->judul_berita }}" style="width: 1000px; height: 400px;"></div>
                             <span class="post-date">{{ \Carbon\Carbon::parse($item->tgl_berita)->locale('id')
@@ -36,13 +35,6 @@
                             <p>{{ $item->deskripsi_berita }}</p>
                             <a href="{{ route('detailberita.show', ['id_berita' => $item->id_berita]) }}" class="readmore 
                             stretched-link mt-auto"><span>Baca Selengkapnya</span><i
-=======
-                            <div class="post-img"><img src="{{ asset('img-berita/' . $item->foto_artikel_berita) }}" class="img-fluid" alt="{{ $item->judul_berita }}" style="width: 1000px; height: 400px;"></div>
-                            <span class="post-date">{{ \Carbon\Carbon::parse($item->tgl_berita)->locale('id')->isoFormat('dddd, D MMMM YYYY') }}</span>
-                            <h3 class="cc">{{ $item->judul_berita }}</h3>
-                            <p>{{ Str::limit($item->deskripsi_berita, 100, '...') }}</p>
-                            <a href="{{ route('detailberita.show', ['id_berita' => $item->id_berita]) }}" class="readmore stretched-link mt-auto"><span>Baca Selengkapnya</span><i
->>>>>>> 6dbf9903d4125ad4c0fe223a4d298b99bdb9f28e
                                     class="bi bi-arrow-right"></i></a>
                         </div>
                     </div>

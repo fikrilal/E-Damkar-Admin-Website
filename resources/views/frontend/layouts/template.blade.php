2a11d56dfc6c5f002d6f86f4518a153b39884e8b
ahmadansori1 <e41210577@student.polije.ac.id>
2023-05-19 14:48:36 +0700

    update title

--- resources/views/frontend/layouts/template.blade.php
+++ resources/views/frontend/layouts/template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>{{ $title ?? config('app.name') }}</title>
+    <title>{{ $title ?? config('app.name') }} | E-Damkar Nganjuk</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 

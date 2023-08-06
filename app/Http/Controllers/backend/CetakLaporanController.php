fa4f598cf22610f2451e01626a9fc56c79be24ee
ahmadansori7 <ahmadansori1111@gmail.com>
2023-08-06 08:04:53 +0700

    update

--- app/Http/Controllers/backend/CetakLaporanController.php
+++ app/Http/Controllers/backend/CetakLaporanController.php
@@ -15,27 +15,25 @@ class CetakLaporanController extends Controller
     }
 
 
-
     public function show(Request $request)
     {
+        $title = 'Cetak Laporan Kebakaran | E-Damkar Nganjuk';
+
         $tanggal = Carbon::now()->locale('id')->isoFormat('DD MMMM YYYY'); // Format tanggal dalam bahasa Indonesia
     
         $idLaporan = $request->query('idLaporan');
     
         // Use Eloquent to retrieve the laporan object with related user_listdata
-        $laporan = laporan::with('user_listdata')->where('idLaporan', $idLaporan)->first();
+        $laporan = Laporan::where('idLaporan', $idLaporan)->first();
     
         if (!$laporan) {
             // Jika laporan dengan $idLaporan tidak ditemukan, berikan respons sesuai kebutuhan
             return redirect()->back()->with('error', 'Laporan tidak ditemukan.');
         }
     
-        // Check if the related user_listdata exists before accessing its properties
-        $namalengkap = $laporan->user_listdata ? $laporan->user_listdata->namaLengkap : null;
-        $nomorhp = $laporan->user_listdata ? $laporan->user_listdata->noHp : null;
-    
         // Kemudian, kembalikan view yang menampilkan data laporan
-        return view('backend.cetak-laporan', compact('laporan', 'tanggal', 'namalengkap', 'nomorhp'));
+        return view('backend.cetak-laporan', compact('laporan', 'tanggal','title'));
     }
+
     
 }

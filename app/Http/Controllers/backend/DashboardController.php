ff57db28e5cdd3f851783397e1fbefaa9e4d8cd3
Akhdan Robbani <danakhdan12@gmail.com>
2023-09-22 14:57:53 +0700

    adjust on sirine

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -1,47 +1,47 @@
 <?php
 
 namespace App\Http\Controllers\Backend;
+
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 
 use App\Http\Controllers\Controller;
+use App\Models\laporan;
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $title = 'Dashboard | E-Damkar Nganjuk';
-        return view('backend.dashboard', ['title' => $title]);        
+        return view('backend.dashboard', ['title' => $title]);
     }
-    
-    public function getLaporanMasuk()
-{
-    $now = now();
-    $laporanMasukRealtime = DB::table('laporans')
-        ->join('detail_laporan_penggunas', 'id', '=', 'laporans.detail_laporan_pengguna_id')
-        ->whereIn('status_riwayat_id', [1])
-        ->whereDate('detail_laporan_penggunas.tgl_pelaporan', $now->toDateString())->get();
-         // Mengambil data terakhir berdasarkan created_at atau kolom lain yang sesuai
-        
-    return response()->json(['count' => $laporanMasukRealtime -> count()  ]); // Mengembalikan 1 jika ada data, 0 jika tidak ada
-}
 
-    
+    public function getLaporanMasuk()
+    {
+        // $now = now();
+        // $laporanMasukRealtime = DB::table('laporans')
+        //     ->join('detail_laporan_penggunas', 'id', '=', 'laporans.detail_laporan_pengguna_id')
+        //     ->whereIn('status_riwayat_id', [1])
+        //     ->whereDate('detail_laporan_penggunas.tgl_pelaporan', $now->toDateString())->get();
+        // Mengambil data terakhir berdasarkan created_at atau kolom lain yang sesuai
+        $laporanMasukRealtime = laporan::Where("status_riwayat_id", 1)->get();
+        return response()->json(['count' => $laporanMasukRealtime->count()]); // Mengembalikan 1 jika ada data, 0 jika tidak ada
+    }
+
+
 
     public function getLaporanKategori()
-{
-    $laporanMasukRealtimeKategori = DB::table('laporans')
-        ->whereIn('status_riwayat_id', [1])
-        ->select('kategori_laporan_id') // Hanya ambil kolom kategori_laporan_id
-        ->orderBy('idLaporan', 'desc') // Urutkan berdasarkan idLaporan secara descending
-        ->first();
+    {
+        $laporanMasukRealtimeKategori = DB::table('laporans')
+            ->whereIn('status_riwayat_id', [1])
+            ->select('kategori_laporan_id') // Hanya ambil kolom kategori_laporan_id
+            ->orderBy('idLaporan', 'desc') // Urutkan berdasarkan idLaporan secara descending
+            ->first();
 
-    $kategori = $laporanMasukRealtimeKategori ? $laporanMasukRealtimeKategori->kategori_laporan_id : '';
+        $kategori = $laporanMasukRealtimeKategori ? $laporanMasukRealtimeKategori->kategori_laporan_id : '';
 
-    return response()->json(['kategori' => $kategori]);
-}
-
-
-
+        return response()->json(['kategori' => $kategori]);
+    }
 }
